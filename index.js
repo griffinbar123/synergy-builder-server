54f7b44aa57b7a445b3e8cd276876c2cb03ba6b3
Griffin Barnard <griffinbarnard@Griffins-MacBook-Air.local>
2024-04-09 23:29:15 -0500

    debug

--- index.js
+++ index.js
@@ -29,6 +29,84 @@ function isJsonString(str) {
 }
 
 
+async function getRawChampionData(name){
+    file = "./champions/" + name + ".json"
+
+    var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+    return obj
+}
+
+function flatten(data, sub){
+    for (const [key, value] of Object.entries(data[sub])) {
+        data[key] = data[sub][key]
+    }
+    delete data[sub]
+}
+
+
+async function getCleanChampionData(name, tier){
+    let data = await getRawChampionData(name)
+    tier = tier.toUpperCase()
+
+    delete data["name"]
+    delete data["id"]
+    delete data["key"]
+    delete data["partype"]
+    flatten(data, "info")
+    flatten(data, "stats")
+    for (const [key, value] of Object.entries(data["gamestats"][tier])) {
+        data[key] = data["gamestats"][tier][key]
+    }
+    delete data["gamestats"]
+
+    data["Tank"] = data["tags"].filter((tag) => tag == "Tank").length
+    data["Fighter"] = data["tags"].filter((tag) => tag == "Fighter").length
+    data["Assassin"] = data["tags"].filter((tag) => tag == "Assassin").length
+    data["Mage"] = data["tags"].filter((tag) => tag == "Mage").length
+    data["Support"] = data["tags"].filter((tag) => tag == "Support").length
+    data["Marksman"] = data["tags"].filter((tag) => tag == "Marksman").length
+
+    delete data["tags"]
+
+    while (true){
+        for (const [key, value] of Object.entries(data)) {
+            if(key.substring(0, "total_".length) === "total_" || key.substring(0, "frequency_".length) === "frequency_"){
+                delete data[key]
+                break
+            }
+        }
+
+        let b = true
+        for (const [key, value] of Object.entries(data)) {
+            if(key.substring(0, "total_".length) === "total_" || key.substring(0, "frequency_".length) === "frequency_"){
+                b = false
+                break
+            }
+        }
+        if(b){
+            break
+        }
+    }
+    
+    delete data["gamesrecorded"]
+
+    return data
+}
+
+async function get_match(t, participants){
+    let jsm = {"tier": t}
+    for(let i = 0; i < participants.length; i++) {
+        let base_n = "participant_" + (i + 1).toString() + "_"
+        let clean_p = await getCleanChampionData(participants[i], t)
+        for (const [key, value] of Object.entries(clean_p)) {
+            jsm[base_n + key] = value
+        }
+    }
+    return jsm
+}
+
+
 app.post("/api/champs", async (req, res, next) => {
     console.log(req.body);
     // await sleep(1000);
@@ -55,26 +133,20 @@ app.post("/api/champs", async (req, res, next) => {
          });
     }
 
-    var d = "";
-    const pythonProcess = spawn('python3',["./fill_files.py", req.body["tier"], req.body["participants"][0], req.body["participants"][1], req.body["participants"][2], req.body["participants"][3], req.body["participants"][4], req.body["participants"][5], req.body["participants"][6], req.body["participants"][7], req.body["participants"][8], req.body["participants"][9]]);
-    pythonProcess.stdout.on('data', (data) => {
-        console.log("GETTING DATA:", data.toString());
-        d = d + data.toString();
-       });
-    pythonProcess.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.setHeader('Content-Type', 'application/json');
-        console.log("d:", d)
-        js = null
-        if(isJsonString(d)) {
-            js = JSON.parse(d)
-        }
-        jsn = {"win": 0.25, "status_code": 200}
-        console.log("sending response: ", jsn)
-        // js.status = 200
-        res.send(jsn);
-    });
+    var d = await get_match(req.body["tier"], [req.body["participants"][0], req.body["participants"][1], req.body["participants"][2], req.body["participants"][3], req.body["participants"][4], req.body["participants"][5], req.body["participants"][6], req.body["participants"][7], req.body["participants"][8], req.body["participants"][9]])
+
+    // send data to browser
+    res.setHeader('Content-Type', 'application/json');
+    console.log("d:", d)
+    js = null
+    if(isJsonString(d)) {
+        js = JSON.parse(d)
+    }
+    jsn = {"win": 0.25, "status_code": 200}
+    console.log("sending response: ", jsn)
+    // js.status = 200
+    res.send(jsn);
+    
 
     // res.end()
 });
